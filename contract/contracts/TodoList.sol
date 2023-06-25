684421ecd36b399635a2b6e1b0db834de8673ed8
anoopraju31 <anoopraju31@gmail.com>
2023-06-25 10:56:10 +0530

    Smart Contract: todolist - create todo function added with create todo event

--- contract/contracts/TodoList.sol
+++ contract/contracts/TodoList.sol
@@ -19,4 +19,35 @@ contract TodoList {
     }
 
     mapping(address => Todo[]) private todos;
+
+    event TodoCreate(address _owner, uint _id);
+
+    /**
+     * @dev create a new todo
+     * @param _title The title for the todo.
+     * @param _description The description about the todo.
+     * @param _targetTime The target time for the todo.
+     * @param _priority The priority for the todo.
+     */
+
+    function createTodo(
+        string memory _title,
+        string memory _description,
+        uint _targetTime,
+        uint8 _priority
+    ) public {
+        Todo memory todo;
+
+        todo.title = _title;
+        todo.description = _description;
+        todo.createdTime = block.timestamp;
+        todo.lastUpdated = block.timestamp;
+        todo.targetTime = _targetTime;
+        todo.priority = _priority;
+        todo.isPresent = true;
+
+        todos[msg.sender].push(todo);
+
+        emit TodoCreate(msg.sender, todos[msg.sender].length);
+    }
 }
