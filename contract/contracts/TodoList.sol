ee81a6cc6b95ca91f3905f71b83f7563ae530a02
anoopraju31 <anoopraju31@gmail.com>
2023-06-25 10:49:39 +0530

    Smart Contract: todolist - todo struct created

--- contract/contracts/TodoList.sol
+++ contract/contracts/TodoList.sol
@@ -1,4 +1,22 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-contract TodoList {}
+/**
+ * @title TodoList
+ * @author Anoop Raju
+ * @dev A simple todo list smart contract
+ */
+contract TodoList {
+    struct Todo {
+        string title;
+        string description;
+        uint createdTime;
+        uint lastUpdated;
+        uint targetTime;
+        uint8 priority;
+        bool status;
+        bool isPresent;
+    }
+
+    mapping(address => Todo[]) private todos;
+}
