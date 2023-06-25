607a66e9156a0aab0cbb2838cb147a9c2700e4b2
anoopraju31 <anoopraju31@gmail.com>
2023-06-25 14:24:26 +0530

    Todo List Dapp: Basic Web3 Config added

--- dapp/src/index.js
+++ dapp/src/index.js
@@ -1,7 +1,42 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import './index.css'
+import { getDefaultWallets, RainbowKitProvider } from '@rainbow-me/rainbowkit'
+import { chain, configureChains, createClient, WagmiConfig } from 'wagmi'
+import { alchemyProvider } from 'wagmi/providers/alchemy'
+import { publicProvider } from 'wagmi/providers/public'
+import { BrowserRouter } from 'react-router-dom'
+
 import App from './App'
 
+import '@rainbow-me/rainbowkit/styles.css'
+import './index.css'
+
+const { chains, provider } = configureChains(
+	[chain.sepolia],
+	[
+		alchemyProvider({ apiKey: process.env.REACT_APP_ALCHEMY_ID }),
+		publicProvider(),
+	],
+)
+const { connectors } = getDefaultWallets({
+	appName: 'Todo List Dapp',
+	chains,
+})
+const wagmiClient = createClient({
+	autoConnect: true,
+	connectors,
+	provider,
+})
+
 const root = ReactDOM.createRoot(document.getElementById('root'))
-root.render(<App />)
+root.render(
+	<React.StrictMode>
+		<WagmiConfig client={wagmiClient}>
+			<RainbowKitProvider chains={chains} coolMode>
+				<BrowserRouter>
+					<App />
+				</BrowserRouter>
+			</RainbowKitProvider>
+		</WagmiConfig>
+	</React.StrictMode>,
+)
