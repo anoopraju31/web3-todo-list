7bf1d9f51af9f730e0e31c8af0d49f068c4544de
anoopraju31 <anoopraju31@gmail.com>
2023-06-25 15:39:50 +0530

    Todo List Dapp: Navbar toggle function added

--- dapp/src/components/Navbar.js
+++ dapp/src/components/Navbar.js
@@ -1,23 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useWidth } from '../hooks'
 
 const Navbar = () => {
+	const [open, setOpen] = useState(false)
+	const currentWidth = useWidth()
+
 	return (
 		<nav className='bg-white dark:bg-gray-900 fixed w-full z-20 top-0 left-0 border-b border-gray-200 dark:border-gray-600'>
-			<div className='max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4'>
-				<a href='https://flowbite.com/' className='flex items-center'>
-					<img
-						src='https://flowbite.com/docs/images/logo.svg'
-						className='h-8 mr-3'
-						alt='Flowbite Logo'
-					/>
+			<div className='max-w-screen-2xl flex flex-wrap items-center justify-between mx-auto py-4 px-8'>
+				<a href='/' className='flex items-center'>
 					<span className='self-center text-2xl font-semibold whitespace-nowrap dark:text-white'>
-						Flowbite
+						ETHTODO
 					</span>
 				</a>
 				<div className='flex md:hidden md:order-2'>
 					<button
 						data-collapse-toggle='navbar-sticky'
 						type='button'
+						onClick={() => setOpen((prev) => !prev)}
 						className='inline-flex items-center p-2 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600'
 						aria-controls='navbar-sticky'
 						aria-expanded='false'>
@@ -35,7 +35,10 @@ const Navbar = () => {
 						</svg>
 					</button>
 				</div>
-				<div className='items-center justify-between w-full md:flex md:w-auto md:order-1'>
+				<div
+					className={`items-center justify-between w-full ${
+						currentWidth < 768 && open ? 'block' : 'hidden'
+					} md:flex md:w-auto md:order-1`}>
 					<ul className='flex flex-col gap-4 md:gap-0 p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-6 md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700'>
 						<li>
 							<button
--- dapp/src/index.js
+++ dapp/src/index.js
@@ -1,10 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import {
-	ConnectButton,
-	getDefaultWallets,
-	RainbowKitProvider,
-} from '@rainbow-me/rainbowkit'
+import { getDefaultWallets, RainbowKitProvider } from '@rainbow-me/rainbowkit'
 import { chain, configureChains, createClient, WagmiConfig } from 'wagmi'
 import { alchemyProvider } from 'wagmi/providers/alchemy'
 import { publicProvider } from 'wagmi/providers/public'
