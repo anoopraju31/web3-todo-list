4e27b25cef208043077d684f42b587a5654a9555
anoopraju31 <anoopraju31@gmail.com>
2023-06-25 22:33:14 +0530

    Todo List Dapp: Card Component - initial

--- dapp/src/App.js
+++ dapp/src/App.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Route, Routes } from 'react-router-dom'
 import { Error, Home, Login } from './pages'
-import Navbar from './components/Navbar'
+import { Navbar } from './components'
 
 const App = () => {
 	return (
