ceb3eb8775e69ef45a68e15bec6298ea3197d538
anoopraju31 <anoopraju31@gmail.com>
2023-06-25 11:30:13 +0530

    Smart Contract: Testing todolist - returns all todos of user1

--- contract/test/todoList.js
+++ contract/test/todoList.js
@@ -21,5 +21,15 @@ describe('Todo List Smart Contract', () => {
 				.to.emit(todoList, 'TodoCreate')
 				.withArgs(user1.address, 1)
 		})
+
+		it('Should return all the todos of user1', async () => {
+			const todo = await todoList.getTodos()
+
+			expect(todos[0].title).to.be.eq(todo[0].title)
+			expect(todos[0].description).to.be.eq(todo[0].description)
+			expect(todos[0].targetTime).to.be.eq(todo[0].targetTime)
+			expect(todos[0].priority).to.be.eq(todo[0].priority)
+			expect(todos[0].status).to.be.eq(todo[0].false)
+		})
 	})
 })
