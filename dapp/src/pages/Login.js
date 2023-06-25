f9d1dac222a2228e1b7b505623143f91bfbb290a
anoopraju31 <anoopraju31@gmail.com>
2023-06-25 16:35:17 +0530

    Todo List Dapp: Login Page navigate to connect added

--- dapp/src/pages/Login.js
+++ dapp/src/pages/Login.js
@@ -1,6 +1,16 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useNavigate } from 'react-router-dom'
+import { useAccount, useSigner } from 'wagmi'
 
 const Login = () => {
+	const navigate = useNavigate()
+	const { isConnected } = useAccount()
+	const { data: signer } = useSigner()
+
+	useEffect(() => {
+		if (isConnected) navigate('/')
+	}, [signer])
+
 	return <div>Login</div>
 }
 
