4324589dffbffe1700a02ecedf418c7ed49e89ec
anoopraju31 <anoopraju31@gmail.com>
2023-06-25 11:07:43 +0530

    Smart Contract: Testing todolist - before added

--- contract/test/todoList.js
+++ contract/test/todoList.js
@@ -1,4 +1,12 @@
 const { expect } = require('chai')
-const { ether } = require('hardhat')
+const { ethers } = require('hardhat')
 
-describe('Todo List Smart Contract', () => {})
+describe('Todo List Smart Contract', () => {
+	let user1, todoList
+
+	before(async () => {
+		;[user1] = await ethers.getSigners()
+		const TodoList = await ethers.getContractFactory('TodoList')
+		todoList = await TodoList.deploy()
+	})
+})
