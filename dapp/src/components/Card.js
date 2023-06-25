e415dc945b949d7a109c87cabf18c7db08888235
anoopraju31 <anoopraju31@gmail.com>
2023-06-25 22:47:46 +0530

    Todo List Dapp: Card Component - styled

--- dapp/src/components/Card.js
+++ dapp/src/components/Card.js
@@ -1,7 +1,28 @@
 import React from 'react'
+import { FaRegEdit } from 'react-icons/fa'
 
-const Card = () => {
-	return <div>Card</div>
+const Card = ({ title, description, createdTime, targetTime }) => {
+	return (
+		<div className='relative w-full p-4  border border-gray-200 rounded shadow  dark:border-gray-700 bg-green-800 bg-opacity-50'>
+			<div className='p-2 absolute top-2 right-2 flex items-center justify-center round_border text-white hover:bg-gray-100 focus:outline-none focus:ring-1 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600'>
+				<FaRegEdit size={18} />
+			</div>
+			<h5 className='mb-2 w-[80%] font-mono font-medium tracking-tighter text-gray-100 dark:text-white'>
+				{title}
+			</h5>
+			<p className='mb-3 font-mono text-sm  leading-5 text-gray-300 dark:text-gray-400'>
+				{description}
+			</p>
+			<div className='flex gap-3'>
+				<p className='mb-3 font-normal text-gray-400 dark:text-gray-400'>
+					{createdTime}
+				</p>
+				<p className='mb-3 font-normal text-gray-400 dark:text-gray-400'>
+					{targetTime}
+				</p>
+			</div>
+		</div>
+	)
 }
 
 export default Card
--- dapp/src/pages/Home.js
+++ dapp/src/pages/Home.js
@@ -2,6 +2,7 @@ import React, { useEffect } from 'react'
 import { useNavigate } from 'react-router-dom'
 import { useAccount, useSigner } from 'wagmi'
 import useTodo from '../hooks/useTodo'
+import { Card } from '../components'
 
 const Home = () => {
 	const navigate = useNavigate()
@@ -13,7 +14,18 @@ const Home = () => {
 		if (!isConnected) navigate('/connect')
 	}, [signer])
 
-	return <div className='mt-32'>{JSON.stringify(todos)}</div>
+	return (
+		<div className='mt-20 min-h-screen bg-white dark:bg-gray-900 '>
+			{todos?.map((todo) => (
+				<Card
+					title={todo.title}
+					description={todo.description}
+					createdTime={Number(todo.createdTime._hex)}
+					targetTime={Number(todo.targetTime._hex)}
+				/>
+			))}
+		</div>
+	)
 }
 
 export default Home
