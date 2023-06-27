89791298d2d065824e4fb3067696fc040f5eb266
anoopraju31 <anoopraju31@gmail.com>
2023-06-27 17:41:52 +0530

    todoList Dapp: Navbar Component - comments added

--- dapp/src/components/Navbar.js
+++ dapp/src/components/Navbar.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { useDispatch } from 'react-redux'
 import { useWidth } from '../hooks'
 import { ConnectButton } from '@rainbow-me/rainbowkit'
@@ -10,9 +10,11 @@ import {
 
 const Navbar = () => {
 	const [open, setOpen] = useState(false)
+	const [theme, setTheme] = useState('light')
 	const currentWidth = useWidth()
 	const dispatch = useDispatch()
 
+	// function to open model form
 	const handleClick = () => {
 		dispatch(switchToCreateForm())
 		dispatch(
@@ -27,15 +29,31 @@ const Navbar = () => {
 		)
 		dispatch(toggleModel(true))
 	}
+
+	// theme change
+	const handleTheme = () => {
+		setTheme((prev) => {
+			return prev === 'dark' ? 'light' : 'dark'
+		})
+	}
+
+	useEffect(() => {
+		if (theme === 'dark') document.documentElement.classList.add(theme)
+		else document.documentElement.classList.remove('dark')
+		localStorage.theme = theme
+	}, [theme])
+
 	return (
 		<nav className='bg-white dark:bg-gray-900 fixed w-full z-20 top-0 left-0 border-b border-gray-200 dark:border-gray-600'>
 			<div className='max-w-screen-2xl flex flex-wrap items-center justify-between mx-auto py-4 px-8 md:px-16'>
+				{/* Logo */}
 				<a href='/' className='flex items-center'>
 					<span className='self-center text-2xl font-semibold whitespace-nowrap dark:text-white'>
 						ETHTODO
 					</span>
 				</a>
 				<div className='flex md:hidden md:order-2'>
+					{/* Menu Button */}
 					<button
 						data-collapse-toggle='navbar-sticky'
 						type='button'
@@ -72,9 +90,19 @@ const Navbar = () => {
 							</button>
 						</li>
 
+						{/* Theme Button */}
+						<li>
+							<div
+								// type='button'
+								onClick={handleTheme}
+								className='w-full md:w-[102px] flex-1 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-[10px] text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800'>
+								Theme
+							</div>
+						</li>
+
 						{/* Wallet Connect Button */}
 						<li className='w-full'>
-							<ConnectButton showBalance={false} />
+							<ConnectButton chainStatus='full' showBalance={false} />
 						</li>
 					</ul>
 				</div>
